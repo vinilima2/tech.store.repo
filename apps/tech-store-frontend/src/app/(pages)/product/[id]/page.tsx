2d1f90142cab67177188495510c497b8f1e9b7a4
Vinicius Lima <vinicius.limavl001@gmail.com>
2025-02-24 21:35:31 -0300

    fix: Showing the real product when selected.

--- apps/tech-store-frontend/src/app/(pages)/product/[id]/page.tsx
+++ apps/tech-store-frontend/src/app/(pages)/product/[id]/page.tsx
@@ -1,26 +1,30 @@
+'use client'
 import ProductInfo from "@/components/product/ProductInfo";
 import ProductNotFound from "@/components/product/ProductNotFound";
 import ProductTitle from "@/components/product/ProductTitle";
 import PurchaseBanner from "@/components/product/PurchaseBanner";
-import {products} from "@tstore/core";
+import { products } from "@tstore/core";
 import PriceMeter from "@/components/product/PriceMeter";
 import UserReview from "@/components/product/UserReview";
 import SpecialReview from "@/components/product/SpecialReview";
+import useProducts from "@/data/hooks/useProducts";
 
 export default function ProductPage(props: any) {
     const id = +props.params.id;
-    const product = products.find((p) => p.id = id)
+    const { findById } = useProducts();
+    const product = findById(id)
+    
     return product ? (
-            <div className="flex flex-col gap-20 container py-10">
-                <div className="flex flex-col gap-10">
-                    <ProductTitle product={product}/>
-                    <ProductInfo product={product}/>
-                    <PurchaseBanner product={product}/>
-                    <PriceMeter product={product}/>
-                </div>
-                <UserReview product={product}/>
-                <SpecialReview product={product}/>
-            </div>)
-        : (<ProductNotFound/>)
+        <div className="flex flex-col gap-20 container py-10">
+            <div className="flex flex-col gap-10">
+                <ProductTitle product={product} />
+                <ProductInfo product={product} />
+                <PurchaseBanner product={product} />
+                <PriceMeter product={product} />
+            </div>
+            <UserReview product={product} />
+            <SpecialReview product={product} />
+        </div>)
+        : (<ProductNotFound />)
 
 }
